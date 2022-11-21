6d7c62a8cbb046025bfb99b7862dfb05b076a42f
Melisa Şahin <111955439+melisasahinn@users.noreply.github.com>
2022-11-21 21:44:06 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+// data yönetilen yer
+@Component({ //html'nin datasını yönettiğimiz yer {}:producy.class gibi obje demek
+  selector: 'app-root',// <app-root></app-root> bağlantısının görüldüğü yer
+  templateUrl: './app.component.html', //./ aynı klasörde bulunan html
+  styleUrls: ['./app.component.css']//köşeli parantez .js dünaysında array demek
 })
 export class AppComponent {
-  title = 'northwind';
+  title:string = 'northwind'; //veri türü güvenli hale geldi.
+  user:string='Melisa Şahin'
+
+
 }
